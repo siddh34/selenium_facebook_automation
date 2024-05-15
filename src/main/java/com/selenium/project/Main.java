5370044899b4b42d878da43bc672ff141a18c9cd
siddh34 <ssiddharthsutar@gmail.com>
2024-05-15 20:50:21 +0530

    using firefox

--- src/main/java/com/selenium/project/Main.java
+++ src/main/java/com/selenium/project/Main.java
@@ -1,12 +1,12 @@
 package com.selenium.project;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "./driver/geckodriver");
+        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.google.com");
         System.out.println(driver.getTitle());
         driver.quit();
